fa10c5e5321293a20ea909e24aaf4f5613451a71
Max V Grushin <support@ids.spb.ru>
2024-02-12 11:06:19 +0300

    fix popular container width

--- src/assets/scripts/page/home.js
+++ src/assets/scripts/page/home.js
@@ -1,13 +1,51 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
   // Optional parameters
-  slidesPerView: 6,
+  slidesPerView: 'auto',
   spaceBetween: 0,
   lazy: true,
+  centerInsufficientSlides: true,
   autoplay: {
     delay: 5000,
   },
   loop: true,
   // speed: 800,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  lazy: true,
+  centerInsufficientSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  lazy: true,
+  centerInsufficientSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
